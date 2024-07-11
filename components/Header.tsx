b1695a8595f4d191ee3e5dc2a75c46a5bf50e6f0
nicoryy <pedronicory@gmail.com>
2024-07-11 11:30:40 -0300

    added links and currentPath style feat

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { FaInstagram, FaGithub, FaTwitter } from "react-icons/fa";
 import { CgProfile, CgShoppingCart } from "react-icons/cg";
@@ -5,12 +7,18 @@ import Link from "next/link";
 import { Oswald } from "next/font/google";
 import Button from "./Button";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const oswald = Oswald({ subsets: ["latin"] });
 
 const Header = () => {
+  const currentPath = usePathname();
+
   return (
-    <header data-aos='fade-down' className="overflow-hidden border-b-2 border-pp_black w-full">
+    <header
+      data-aos="fade-down"
+      className="overflow-hidden border-b-2 border-pp_black w-full"
+    >
       <section className="text-sm flex justify-between text-white bg-pp_black px-10 py-2 items-center">
         <ul className="flex gap-6">
           <li className="list-disc">Call: +1 487 888 937</li>
@@ -28,9 +36,42 @@ const Header = () => {
       <section className="flex items-center py-5 px-10 justify-between">
         <nav>
           <ul className="flex gap-5 font-medium">
-            <li>Produtos</li>
-            <li>Galeria</li>
-            <li>Contato</li>
+            <li>
+              <Link
+                href={"/shop"}
+                className={`transition duration-1000 ${
+                  currentPath === "/shop"
+                    ? "border-b-2 border-pp_avocado border-opacity-100"
+                    : "border-opacity-10"
+                }`}
+              >
+                Produtos
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/galeria"}
+                className={`transition duration-1000 ${
+                  currentPath === "/galeria"
+                    ? "border-b-2 border-pp_avocado border-opacity-100"
+                    : "border-opacity-10"
+                }`}
+              >
+                Galeria
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/contato"}
+                className={`transition duration-1000 ${
+                  currentPath === "/contato"
+                    ? "border-b-2 border-pp_avocado border-opacity-100"
+                    : "border-opacity-10"
+                }`}
+              >
+                Contato
+              </Link>
+            </li>
           </ul>
         </nav>
 
@@ -49,7 +90,7 @@ const Header = () => {
           <CgProfile size={25} />
           <CgShoppingCart size={25} />
           <Button
-            href={"/"}
+            href={"/shop"}
             className="bg-pp_avocado px-4 py-2 rounded text-pp_white"
           >
             Nossa Loja
