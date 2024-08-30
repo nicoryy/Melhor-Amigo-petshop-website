222460155b110c8eb78b0a1c18070c377988a3c0
nicoryy <pedronicory@gmail.com>
2024-08-29 23:56:18 -0300

    style: header respontivity

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { FaInstagram, FaGithub, FaTwitter } from "react-icons/fa";
 import { CgProfile, CgShoppingCart } from "react-icons/cg";
 import Link from "next/link";
@@ -9,17 +9,25 @@ import Button from "./Button";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 
+import { CgMenu, CgClose } from "react-icons/cg";
+
 const oswald = Oswald({ subsets: ["latin"] });
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   const currentPath = usePathname();
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <header
       data-aos="fade-down"
-      className="overflow-hidden border-b-2 border-pp_black w-full"
+      className="relative border-b-2 border-pp_black w-full overflow-x-hidden"
     >
-      <section className="text-sm flex justify-between text-white bg-pp_black px-10 py-2 items-center">
+      <section className="hidden text-sm lg:flex justify-between text-white bg-pp_black px-10 py-2 items-center">
         <ul className="flex gap-6">
           <li className="list-disc">Call: +1 487 888 937</li>
           <li className="list-disc">Quote : price@domain.com</li>
@@ -33,12 +41,30 @@ const Header = () => {
         </div>
       </section>
 
-      <section className="flex items-center py-5 px-10 justify-between">
+      <section className="flex items-center py-5 px-10 justify-center lg:justify-between">
         <nav>
-          <ul className="flex gap-5 font-medium">
+          <ul
+            className={`z-10 bg-pp_avocado_500 lg:bg-transparent gap-10 text-xl lg:text-base text-white lg:text-black flex flex-col lg:flex-row justify-center items-center fixed m-auto top-0 right-0 left-0 bottom-0 lg:static lg:flex lg:gap-5 font-medium transition duration-1000 ease-in-out
+            ${
+              showMenu
+                ? "translate-y-0"
+                : "lg:inline-block translate-y-[calc(100%+5rem)] lg:translate-y-0"
+            }`}
+          >
+            <CgClose
+              size={50}
+              className="lg:hidden absolute bottom-10 rounded-full p-2 text-pp_avocado_500 bg-white cursor-pointer"
+              onClick={() => {
+                toggleMenu();
+              }}
+            />
+
             <li>
               <Link
                 href={"/shop"}
+                onClick={() => {
+                  toggleMenu();
+                }}
                 className={`transition duration-1000 ${
                   currentPath === "/shop"
                     ? "border-b-2 border-pp_avocado border-opacity-100"
@@ -51,6 +77,9 @@ const Header = () => {
             <li>
               <Link
                 href={"/galeria"}
+                onClick={() => {
+                  toggleMenu();
+                }}
                 className={`transition duration-1000 ${
                   currentPath === "/galeria"
                     ? "border-b-2 border-pp_avocado border-opacity-100"
@@ -63,6 +92,9 @@ const Header = () => {
             <li>
               <Link
                 href={"/contato"}
+                onClick={() => {
+                  toggleMenu();
+                }}
                 className={`transition duration-1000 ${
                   currentPath === "/contato"
                     ? "border-b-2 border-pp_avocado border-opacity-100"
@@ -75,23 +107,24 @@ const Header = () => {
           </ul>
         </nav>
 
-        <h1 className={`font-bold text-2xl ${oswald.className}`}>
-          <Link href={"/"}>
-            <Image
-              src={"/assets/melhor.png"}
-              alt="logo"
-              height={80}
-              width={80}
-            />
-          </Link>
-        </h1>
+        <Link href={"/"}>
+          <Image src={"/assets/melhor.png"} alt="logo" height={80} width={80} />
+        </Link>
 
-        <div className="flex gap-3 items-center">
+        <CgMenu
+          size={30}
+          className="lg:hidden absolute right-10 cursor-pointer"
+          onClick={() => {
+            toggleMenu();
+          }}
+        />
+
+        <div className="flex absolute lg:static left-10 gap-3 items-center">
           <CgProfile size={25} />
           <CgShoppingCart size={25} />
           <Button
             href={"/shop"}
-            className="bg-pp_avocado px-4 py-2 rounded text-pp_white"
+            className="bg-pp_avocado hidden lg:block px-4 py-2 rounded text-pp_white"
           >
             Nossa Loja
           </Button>
